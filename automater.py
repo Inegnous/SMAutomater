cbcadc1a4a6f1860bb13fbba6a1d1bdf0907a0e9
Inegnous <44476609+Inegnous@users.noreply.github.com>
2020-10-23 15:11:46 +0300

    Update automater.py

--- automater.py
+++ automater.py
@@ -22,3 +22,22 @@ class instagram:
 
 
 insta= instagram("*******","*******")
+
+def job():
+    browser = webdriver.Chrome(executable_path='C:\\Games\\chromedriver.exe')
+    insta.login(browser)
+
+    time.sleep(3)
+    message=browser.find_element_by_class_name("xWeGp")
+    message.click()
+    time.sleep(3)
+    notnow=browser.find_element_by_css_selector("button[tabindex='0']")
+    notnow.click() 
+    dev= browser.find_element_by_xpath("//div[text()='_oneredchrysomallos_']")
+    dev.click()
+    time.sleep(3)
+    notnow=browser.find_element_by_css_selector("button[tabindex='0']")
+    notnow.click() 
+    sendmessage=browser.find_element_by_css_selector("textarea[style='height: 18px;']")
+    sendmessage.send_keys('I love you')
+    sendmessage.send_keys(Keys.ENTER)
